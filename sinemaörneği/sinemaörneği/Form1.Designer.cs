9cfa5be748923d0f78edcb3bc3411f6f899a6905
Hakan <Hakan@DESKTOP-FMCAGTH>
2016-12-16 19:58:17 +0300

    devam

---
+++ sinemaörneği/sinemaörneği/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace sinemaörneği
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelkoltukno = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxadsoyad = new System.Windows.Forms.TextBox();
+            this.radioerkek = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.gösterbtn = new System.Windows.Forms.Button();
+            this.kaydetbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Location = new System.Drawing.Point(-5, 361);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(425, 119);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Sırano:";
+            this.columnHeader1.Width = 76;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "adsoyad";
+            this.columnHeader2.Width = 149;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "cinsiyet";
+            this.columnHeader3.Width = 133;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(549, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "koltukno:";
+            // 
+            // labelkoltukno
+            // 
+            this.labelkoltukno.AutoSize = true;
+            this.labelkoltukno.Location = new System.Drawing.Point(677, 73);
+            this.labelkoltukno.Name = "labelkoltukno";
+            this.labelkoltukno.Size = new System.Drawing.Size(24, 17);
+            this.labelkoltukno.TabIndex = 2;
+            this.labelkoltukno.Text = "00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(549, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "adsoyad:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(549, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "cinsiyet:";
+            // 
+            // textBoxadsoyad
+            // 
+            this.textBoxadsoyad.Location = new System.Drawing.Point(648, 127);
+            this.textBoxadsoyad.Name = "textBoxadsoyad";
+            this.textBoxadsoyad.Size = new System.Drawing.Size(148, 22);
+            this.textBoxadsoyad.TabIndex = 5;
+            // 
+            // radioerkek
+            // 
+            this.radioerkek.AutoSize = true;
+            this.radioerkek.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioerkek.Location = new System.Drawing.Point(647, 185);
+            this.radioerkek.Name = "radioerkek";
+            this.radioerkek.Size = new System.Drawing.Size(81, 29);
+            this.radioerkek.TabIndex = 6;
+            this.radioerkek.TabStop = true;
+            this.radioerkek.Text = "erkek";
+            this.radioerkek.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton1.Location = new System.Drawing.Point(734, 187);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(80, 29);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "kadın";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // gösterbtn
+            // 
+            this.gösterbtn.Location = new System.Drawing.Point(554, 309);
+            this.gösterbtn.Name = "gösterbtn";
+            this.gösterbtn.Size = new System.Drawing.Size(102, 42);
+            this.gösterbtn.TabIndex = 8;
+            this.gösterbtn.Text = "Göster";
+            this.gösterbtn.UseVisualStyleBackColor = true;
+            // 
+            // kaydetbtn
+            // 
+            this.kaydetbtn.Location = new System.Drawing.Point(694, 309);
+            this.kaydetbtn.Name = "kaydetbtn";
+            this.kaydetbtn.Size = new System.Drawing.Size(102, 42);
+            this.kaydetbtn.TabIndex = 9;
+            this.kaydetbtn.Text = "kaydet";
+            this.kaydetbtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(873, 492);
+            this.Controls.Add(this.kaydetbtn);
+            this.Controls.Add(this.gösterbtn);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.radioerkek);
+            this.Controls.Add(this.textBoxadsoyad);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelkoltukno);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelkoltukno;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxadsoyad;
+        private System.Windows.Forms.RadioButton radioerkek;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button gösterbtn;
+        private System.Windows.Forms.Button kaydetbtn;
+    }
+}
+
---
+++ sinemaörneği/sinemaörneği/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace sinemaörneği
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglan = new SqlConnection("Data Source=DESKTOP-FMCAGTH;Initial Catalog=sinema;Integrated Security=True");
+        private void verigöster()
+        {
+            baglan.Open();
+            SqlCommand komut = new SqlCommand("Select*from bilet", baglan);
+
+        }
+        int sayac = 48;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if ((i != 4 || j > 1) && (i != 4 || j < 8))
+                    {
+                        Button btn = new Button();
+                        btn.Width = 30;
+                        btn.Height = 30;
+                        btn.Text = sayac.ToString();
+                        btn.BackColor = Color.Green;
+                        btn.Left = (btn.Width * j) + 25;
+                        btn.Top = (btn.Height * i) + 100;
+                        btn.Click += Button_Click;
+                        this.Controls.Add(btn);
+                    }
+                    sayac--;
+
+                }
+            }
+        }
+
+        private void Button_Click(object sender,EventArgs e)
+        {
+            Button koltuk = sender as Button;
+            koltuk.BackColor = Color.Red;
+            labelkoltukno.Text = koltuk.Text;
+
+
+        }
+    }
+}
